76569ce787214f3c645c7d020828fc71591d120c
Eric RAVOAVY <erra_adm@silver-holdings.lan>
2019-11-10 21:59:23 +0100

    feat:define object class userclass

--- New user info.ps1
+++ New user info.ps1
@@ -1,8 +1,16 @@
 ﻿#Creating PSobject model with information properties of a new user.
 #These informations should be provided by HR departement
+#Import all the command module fo managing active directory
+#Import-Module ActiveDirectory
 
 #Import all the command module fo managing active directory
-Import-Module ActiveDirectory
+#ConnectAD.ps1
+#Connect to your Domain Controller(DC)
+#Change the value after the -ComputerName to your know DC
+
+#$session = New-PSSession -ComputerName "VWSERVDCSH" -Credential (Get-Credential)
+#Invoke-Command $session -Scriptblock { Import-Module ActiveDirectory }
+#Import-PSSession -Session $session -module ActiveDirectory
 
 #describe the properties of User object
 $Userclass = New-Object psobject -Property @{
@@ -57,11 +65,14 @@ function Userclass {
 
 echo "la class userclass définissant un utilisateur de l'AD est défini"
 
+
+
 #connexion to the Active directory and create users
 
 #Initiate an user from class Userclass
 $New_User = Userclass -first_name Jean -last_name Alvin -login je.du -Tel_number 0000 -mail_address art@mail.com -departement CA -function VP -OUPath "OU=Laptop Users,OU=Users,OU=SH,DC=silver-holdings,DC=lan" -Enable "$true"
 
+<<<<<<< HEAD
 #display the new User created
 echo $New_User
 
@@ -70,8 +81,16 @@ echo $New_User
 #$Password = ConvertTo-SecureString "Slash7739" -AsPlainText -Force
 #$cred = New-Object System.Management.Automation.PSCredential ("silver-holdings.lan\svc_create_user", $Password)
 #Enter-PSSession –ComputerName VWSERVDCSH.silver-holdings.lan –Credential $cred
+=======
+$New_User = Userclass -first_name Jean -last_name Dupont -login je.du -Tel_number 0000 -mail_address art@mail.com -departement CA -function VP
+
+>>>>>>> dev-userclass
 
 #verifier qu'il n'existe pas de user déjà crée avec le $New_
 
 #Creating the user to the Active Directory
+<<<<<<< HEAD
 New-ADUser -Name $New_User.first_name -GivenName $New_User.first_name -Surname $New_User.lastname  -Department $New_User.departement -Description $New_User.function -OfficePhone $New_User.Tel_number -SamAccountName $New_User.login -EmailAddress $New_User.mail_address -Path $New_User.OUPath -AccountPassword (ConvertTo-SecureString "Welcome.2019" -AsPlainText -force) -PassThru -Enabled $true
+=======
+New-ADUser -Name $New_User.first_name -GivenName $New_User.first_name -Surname $New_User.lastname  -Department $New_User.departement -Description $New_User.function -OfficePhone $New_User.Tel_number -SamAccountName $New_User.login -EmailAddress $New_User.mail_address -path "OU=Laptop Users,OU=Users,OU=SH,DC=silver-holdings,DC=lan" -AccountPassword (ConvertTo-SecureString "Welcome.2019" -AsPlainText -force) -PassThru -Enabled $true
+>>>>>>> dev-userclass
---
+++ README.md
@@ -0,0 +1,1 @@
+# Creation de class d'objet USER qui va servir de modèle pour les nouvelles comptes AD
