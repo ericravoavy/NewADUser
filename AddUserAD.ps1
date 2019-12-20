413a8d2aacc3a7e198107eb5f8d5714887d158a7
eric RAVOAVY <eric.ravoavy@silver-holdings.lan>
2019-12-20 19:30:44 +0100

    Creation utilisateur par powershell

--- AddUserAD.ps1
+++ AddUserAD.ps1
@@ -10,10 +10,23 @@
 #Invoke-Command $session -Scriptblock { Import-Module ActiveDirectory }
 #Import-PSSession -Session $session -module ActiveDirectory
 
+param(
+    $name,
+    $given_name,
+    $last_name,
+    $samaccountname,
+    $login,
+    $Tel_Number,
+    $mail_address
+    #$departement,
+    #$function
+    )
 #describe the properties of Userclass object
 $Userclass = New-Object psobject -Property @{
+    name = $null
     first_name = $null
     last_name = $null
+    samaccountname = $null
     login = $null
     Tel_number = $null
     mail_address = $null
@@ -28,29 +41,35 @@ $Userclass = New-Object psobject -Property @{
 function Userclass {
     param(
         [Parameter(Mandatory=$true)]
+        [String]$name,
+        [Parameter(Mandatory=$true)]
         [String]$first_name,
         [Parameter(Mandatory=$true)]
         [string]$last_name,
         [parameter(Mandatory=$true)]
+        [string]$samaccountname,
+        [parameter(Mandatory=$true)]
         [string]$login,
         [Parameter(Mandatory=$false)]
         [long]$Tel_number,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [string]$mail_address,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         #[ValidateSet('CA','Treasury','IT','Finance')]
         [String]$departement,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         #[ValidateSet('IT','Accountant','VP','AVP')]
         [string]$function,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [string]$OUPath,
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory=$false)]
         [string]$Enable
     )
     $Userclass = $Userclass.psobject.copy()
+    $Userclass.name = $name
     $Userclass.first_name = $first_name
     $Userclass.last_name = $last_name
+    $Userclass.samaccountname = $samaccountname
     $Userclass.login = $login
     $Userclass.Tel_number = $Tel_number
     $Userclass.mail_address = $mail_address
@@ -65,7 +84,7 @@ echo "la class userclass définissant un utilisateur de l'AD est défini"
 
 
 #Initiate an user from class Userclass
-$New_User = Userclass -first_name Jean -last_name Alvin -login je.du -Tel_number 063243567 -mail_address art@mail.com -departement CA -function VP -OUPath "OU=Laptop Users,OU=Users,OU=SH,DC=silver-holdings,DC=lan" -Enable "$true"
+$New_User = Userclass -name $name -first_name $given_name -last_name $last_name -samaccountname $samaccountname -login $login -Tel_number $Tel_Number -mail_address $mail_address -OUPath "OU=Laptop Users,OU=Users,OU=SH,DC=silver-holdings,DC=lan" -Enable "$true"
 
 #display the new User created
 echo $New_User
@@ -77,8 +96,8 @@ echo $New_User
 #Enter-PSSession –ComputerName VWSERVDCSH.silver-holdings.lan –Credential $cred
 
 #connexion to the Active directory and create users
-
-New-ADUser -Name $New_User.first_name -GivenName $New_User.first_name -Surname $New_User.lastname  -Department $New_User.departement -Description $New_User.function -OfficePhone $New_User.Tel_number -SamAccountName $New_User.login -EmailAddress $New_User.mail_address -Path $New_User.OUPath -AccountPassword (ConvertTo-SecureString "Welcome.2019" -AsPlainText -force) -PassThru -Enabled $true
+New-ADUser -Name $New_User.name -GivenName $New_User.first_name -Surname $New_User.last_name -SamAccountName $samaccountname -UserPrincipalName $login -Path $New_User.OUPath -AccountPassword (ConvertTo-SecureString "Welcome.2019" -AsPlainText -force) -PassThru -Enabled $true
+#New-ADUser -Name $New_User.first_name -GivenName $New_User.first_name -Surname $New_User.lastname  -Department $New_User.departement -Description $New_User.function -OfficePhone $New_User.Tel_number -SamAccountName $New_User.login -EmailAddress $New_User.mail_address -Path $New_User.OUPath -AccountPassword (ConvertTo-SecureString "Welcome.2019" -AsPlainText -force) -PassThru -Enabled $true
 
 #verifier qu'il n'existe pas de user déjà crée avec le $New_
 
